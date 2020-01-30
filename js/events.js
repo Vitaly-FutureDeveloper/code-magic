044c5bd7ef75fa9dab15b9f67c85c82d6c77bdbd
Виталий <programmer@hithub.com>
2020-01-30 17:18:15 +0500

    Сделаны обработчики для изменения персонажа и отправки формы с данными

--- js/events.js
+++ js/events.js
@@ -7,6 +7,8 @@ var setupOpen = document.querySelector('.setup-open');
 var setup = document.querySelector('.setup');
 var setupClose = document.querySelector('.setup-close');
 
+//OPEN/CLOSE MODAL >>
+
 var onPopupEscPress = function(evt){
 	if(evt.keyCode === ESC_KEYCODE){
 		closePopup();
@@ -39,6 +41,69 @@ setupClose.addEventListener('keydown', function(evt){
 	if(evt.keyCode === ENTER_KEYCODE)
 		closePopup();
 });
+//OPEN/CLOSE MODAL <<
+
+//CHOOSE COLORS coat, eyes, fireball >>
+
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 
+					'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 
+					'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
+var iterations = {
+	coat: 0,
+	eyes: 0,
+	fireball: 0
+};
+
+var wizardCoat = document.querySelector('.wizard-coat'),
+	coatColor = document.querySelector('input[name=coat-color]');
+
+var wizardEyes = document.querySelector('.wizard-eyes'),
+	eyesColor = document.querySelector('input[name=eyes-color]');
+
+var setupFireballColor =  document.querySelector('.setup-fireball-wrap'),
+	fireballColor =  document.querySelector('.fireball-color');
+
+wizardCoat.addEventListener('click', function(){
+	if( iterations.coat < COAT_COLORS.length - 1 ){
+		iterations.coat++;
+		wizardCoat.style.fill = COAT_COLORS[iterations.coat];
+		coatColor.value = COAT_COLORS[iterations.coat];
+	} else {
+		iterations.coat = 0;
+		wizardCoat.style.fill = COAT_COLORS[iterations.coat];
+		coatColor.value = COAT_COLORS[iterations.coat];
+	}
+});
+
+wizardEyes.addEventListener('click', function(){
+	if( iterations.eyes < EYES_COLORS.length - 1 ){
+		iterations.eyes++;
+		wizardEyes.style.fill = EYES_COLORS[iterations.eyes];
+		eyesColor.value = EYES_COLORS[iterations.eyes];
+	} else {
+		iterations.eyes = 0;
+		wizardEyes.style.fill = EYES_COLORS[iterations.eyes];
+		eyesColor.value = EYES_COLORS[iterations.eyes];
+	}
+});
+
+setupFireballColor.addEventListener('click', function(){
+		if( iterations.fireball < FIREBALL_COLORS.length - 1 ){
+		iterations.fireball++;
+		setupFireballColor.style.backgroundColor = FIREBALL_COLORS[iterations.fireball];
+		fireballColor.value = FIREBALL_COLORS[iterations.fireball];
+	} else {
+		iterations.fireball = 0;
+		setupFireballColor.style.backgroundColor = FIREBALL_COLORS[iterations.fireball];
+		fireballColor.value = FIREBALL_COLORS[iterations.fireball];
+	}
+
+})
+
+//CHOOSE COLORS coat, eyes, fireball <<
 
 //Валидация формы отправки данных
 var userNameInput = setup.querySelector('.setup-user-name');
