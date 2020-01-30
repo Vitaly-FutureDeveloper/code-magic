a90f5ce7742ca8e93ba4c93c428e20d022c82dd5
Виталий <programmer@hithub.com>
2020-01-30 13:53:18 +0500

    Добавлено удаление обработчиков, заменены магические цифры на константы

--- js/events.js
+++ js/events.js
@@ -1,19 +1,26 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
 var setupOpen = document.querySelector('.setup-open');
 var setup = document.querySelector('.setup');
 var setupClose = document.querySelector('.setup-close');
 
+var onPopupEscPress = function(evt){
+	if(evt.keyCode === ESC_KEYCODE){
+		closePopup();
+	}
+};
+
 var openPopup = function(){
 	setup.classList.remove('hidden');
 
-	document.addEventListener('keydown', function(evt){
-		if(evt.keyCode === 27)
-			closePopup();
-	});
+	document.addEventListener('keydown', onPopupEscPress);
 };
 var closePopup = function(){
 	setup.classList.add('hidden');
+	document.removeEventListener('keydown', onPopupEscPress);
 };
 
 setupOpen.addEventListener('click', function(){
@@ -21,7 +28,7 @@ setupOpen.addEventListener('click', function(){
 });
 
 setupOpen.addEventListener('keydown', function(evt){
-	if(evt.keyCode === 13)
+	if(evt.keyCode === ENTER_KEYCODE)
 		openPopup();
 });
 
@@ -29,7 +36,7 @@ setupClose.addEventListener('click', function(){
 	closePopup();
 });
 setupClose.addEventListener('keydown', function(evt){
-	if(evt.keyCode === 13)
+	if(evt.keyCode === ENTER_KEYCODE)
 		closePopup();
 });
 
