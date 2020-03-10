ac5118553f46c6879523d5234ea1a19f0b3dacd2
Vitaly-Future-Developer <zarvoron@yandex.ru>
2020-03-10 21:23:15 +0500

    Добавлен объект форма (исправлена ошибка)

--- js/backend.js
+++ js/backend.js
@@ -1,10 +1,10 @@
 'use strict';
 
+var data = new FormData(document.querySelector('.setup-wizard-form'));
+
 var onError = function (message){
 	console.error(message);
 };
 var onSuccess = function (data){
 	console.log(data);
 };
-
-
